503cba6cfa454cfa5a65c0797a7642e75d89df1d
brx86 <brx86@qq.com>
2025-06-22 18:34:33 +0800

    add eget config

---
+++ configs/home/aya/eget_syu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+eget    sharkdp/bat
+eget    muesli/duf
+eget    zyedidia/eget
+eget    fastfetch-cli/fastfetch
+eget    junegunn/fzf
+eget    dundee/gdu
+eget    upx/upx
+eget    astral-sh/uv
+
+upx ~/.local/bin/*
