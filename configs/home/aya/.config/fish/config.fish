9be66fe1deb065fbe3e4819e2ead4ceb0dc1f9da
brx86 <brx86@qq.com>
2025-12-01 14:31:10 +0800

    update config.fish

--- configs/home/aya/.config/fish/config.fish
+++ configs/home/aya/.config/fish/config.fish
@@ -3,22 +3,22 @@ if status is-interactive
     set -g PATH $HOME/.local/bin $PATH
     export EDITOR=nano
     export BAT_THEME=OneHalfDark
-    alias ll	'exa -lh --time-style long-iso --icons'
-    alias clip	'curl -F file=@- https://envs.sh'
-    abbr ba	'bat -Sn'
-    abbr c	'cd ..'
-    abbr cip	'curl ip.shakaianee.top'
-    abbr dk	'docker'
-    abbr up	'docker compose up'
-    abbr down	'docker compose down'
-    abbr logs	'docker compose logs -f'
-    abbr m	'micro'
-    abbr pa	'sudo pacman -S'
-    abbr se	'sudo -e'
-    abbr sys	'sudo systemctl'
-    abbr st	'systemctl status'
-    abbr s	'source .venv/bin/activate.fish'
-    abbr uvi	'uv pip install'
+    alias ll    'exa -lh --time-style long-iso --icons'
+    alias clip  'curl -F file=@- https://envs.sh'
+    abbr ba     'bat -Sn'
+    abbr c      'cd ..'
+    abbr cip    'curl ip.shakaianee.top'
+    abbr dk     'docker'
+    abbr up     'docker compose up'
+    abbr down   'docker compose down'
+    abbr logs   'docker compose logs -f'
+    abbr m      'micro'
+    abbr pa     'sudo pacman -S'
+    abbr se     'sudo -e'
+    abbr sys    'sudo systemctl'
+    abbr st     'systemctl status'
+    abbr s      'source .venv/bin/activate.fish'
+    abbr uvi    'uv pip install'
     zoxide init fish | source
     # 在 pwd 中包含 "odin" 字符串时，Ctrl+s 执行 odinfmt 并执行当前命令
     # 否则，Ctrl+s 执行默认的 forward-search-history
@@ -26,7 +26,22 @@ if status is-interactive
         if string match -q "*odin*" (pwd)
             commandline -r "odinfmt -w"
             commandline -f execute
+        else if string match -q "*er_ln*" (pwd)
+            commandline -r "gleam format && gleam run"
+            commandline -f execute
         end
     '
+    bind \cb '
+        if string match -q "*er_ln*" (pwd)
+            commandline -r "gleam run -m gleescript"
+            commandline -f execute
+        end
+    '
+    bind ctrl-c cancel-commandline
+
+    function fish_command_not_found
+        pacfiles $argv[1] || echo -e "$argv[1] not found!"
+    end
+
     bind ctrl-c cancel-commandline
 end
